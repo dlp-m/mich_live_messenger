bcbd20ae56dc9c2f34bfd97fb11f1f5d1908b162
dlp-m <delpierre.michel@outlook.fr>
2026-04-09 23:43:36 +0200

    feat: add WLM-style registration page with scoped Devise views
    - Registration form styled with WindowComponent (Vista Aero chrome)
    - Custom Users::RegistrationsController and SessionsController
      with application layout and username strong param
    - Devise scoped_views enabled so users/ views take precedence over tybo gem

---
+++ config/initializers/devise.rb
@@ -0,0 +1,4 @@
+Devise.setup do |config|
+  # Look for views in app/views/users/ instead of app/views/devise/
+  config.scoped_views = true
+end
--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,10 @@ Rails.application.routes.draw do
       end
   end
   devise_for :administrators, path: "administrators"
-  devise_for :users
+  devise_for :users, controllers: {
+    sessions: "users/sessions",
+    registrations: "users/registrations"
+  }
   mount Tybo::Engine => "/tybo"
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
